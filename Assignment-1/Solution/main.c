9051e57d7df47855ec57d684768420a1f62b68b6
Varun Goyal <varungoyal23@gmail.com>
2019-05-26 19:04:59 +0530

    New files added

---
+++ Assignment-1/Solution/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int companyId;
+    while(scanf("%d", &companyId) != 0 && scanf("%d", &companyId) != EOF){
+        
+    }
+}
