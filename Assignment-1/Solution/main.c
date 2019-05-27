8a0ce2478b718888cbc7c39caa21493a0242367d
Varun Goyal <varungoyal@Varuns-MacBook-Pro-2.local>
2019-05-28 00:39:39 +0530

    Files updated

--- Assignment-1/Solution/main.c
+++ Assignment-1/Solution/main.c
@@ -8,6 +8,7 @@ int scanTime(){
     scanf(":");
     int minutes;
     scanf("%d", &minutes);
+    printf("%d %d\n", hours, minutes);
     int time = hours * 60 + minutes;
     return time;
 }
@@ -19,26 +20,48 @@ struct Node {
     struct Node* next;
     struct Node* prev;
 
-} node;
+};
+
+void addNode (struct Node** head, struct Node** tail, int companyId, int startTime, int endTime, float priority) {
+    if (head[companyId] == NULL) {
+        head = newNode(startTime, endTime, priority);
+        tail = head;
+
+    } else if (head == tail) {
+        tail -> next = newNode(startTime, endTime, priority);
+        head -> next = tail -> next;
+        struct Node* temp = tail;
+        tail = tail -> next;
+        tail -> prev = temp;
+    } else {
+        tail -> next = newNode(startTime, endTime, priority);
+        struct Node* temp = tail;
+        tail = tail -> next;
+        tail -> prev = temp;
+    }
+}
 
 int main(){
     struct Node* head[100000];
-    struct Node* last[100000];
+    struct Node* tail[100000];
     
 
     // int A[100];
-    for (int i = 0; i < 100000; ++i){
+    for (int i = 0; i < 100000; ++i) {
         head[i] = NULL;
-        last[i] = NULL;
+        tail[i] = NULL;
     }
     int companyId;
-    while (scanf("%d", &companyId) != 0 && scanf("%d", &companyId) != EOF) {
+    while (scanf("%d", &companyId) != EOF) {
+
         int startTime = scanTime();
         scanf("-");
         int endTime = scanTime();
         float priority;
         scanf("%f", &priority);
-        printf("%d", startTime);
+        // printf("%d", endTime);
+        addNode(head, tail, companyId, startTime, endTime, priority);
+
 
     }
 }
