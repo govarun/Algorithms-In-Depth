17a668a1401f1db20d80a6877855b3190164c34d
Varun Goyal <varungoyal23@gmail.com>
2019-06-17 14:48:39 +0530

    Prefix Table implemented and Template removed

--- Assignment-3/KMP-Implementation.cpp
+++ Assignment-3/KMP-Implementation.cpp
@@ -1,100 +1,52 @@
-//Program.cpp
-//Problem Statement - 
-
 #include <bits/stdc++.h>
-#include <map>
-#include <unordered_map>
-#include <algorithm>
 using namespace std;
 
 //Author: Varun Goyal (@govarun)
 
-#define f(i, n) for(int i=0;i<(n);++i)
-#define fa(i, a, n) for(int i=(a);i<=(n);++i)
-#define fd(i, a, n) for(int i=(a);i>=(n);--i)
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-#define fill(a)  memset(a, 0, sizeof (a))
-#define ff first
-#define ss second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define ull unsigned long long
-#define debug cerr
-#define d0(x) debug <<#x<<" = "<<x<<' '
-#define d1(x) debug <<"> "<<#x<<" = "<<x<<'\n'
-#define d2(x, y) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<'\n'
-#define d3(x, y, z) debug <<"> "<<#x<<" = "<<x<<' '<<#y<<" = "<<y<<' '<<#z<<" = "<<z<<'\n'
-#define endl '\n'
-typedef long double ld;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector< pair<int, int> > vpii;
-inline bool EQ(double a, double b) { 
-return fabs(a-b) < 1e-9; }
-inline int two(int n) { return 1 << n; }
+void createPrefixTable(string s, vector<int> &v1) {
+    int sizePattern = s.size();
+    v1[0] = 0;
+    v1[1] = 0;
+    for (int j = 2; j <= sizePattern; ++j) {
+        int i = 1;
+        while (i < j) {
+            while (s[j] == s[i] && i < sizePattern && j <= sizePattern) { // j < i
+                v1[j] = i;
+                ++i; 
+                ++j;
+            }
+            if (i >= sizePattern || j > sizePattern) {
+                break;
+            }
+            if (s[j] != s[i] && i == 1) {
+                v1[j] = 0;
+                break;
+            }
+            else {
+                i = 1;
+            }
 
-const ll mod = 1e9 + 7;
-const int maxn = 1e5 + 5;
-
-// vector <int> adj[maxn];
-// bool vis[maxn];
-// int depth[maxn];
-// int p[maxn];
-// queue <int> q;
-
-void pre(){
-
-    
+        }
+    }
 }
 
-
-void solve();
 
 int main(){
-    ios::sync_with_stdio(0);    
-    cin.tie(0); cout.tie(0);
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    int TC = 1;
-    // cin >> TC;
-    while(TC--){    
-        solve();
-    }
-
-    return 0;
-}
-
-void createPrefixTable(String s, vector<int> &v1) {
-	int sizePattern = sPattern.size();
-	v1[0] = 0;
-	v1[1] = 0;
-	for (int i = 1; i < sizePattern; ++i) {
-		for (int j = 2; j <= sizePattern; ++j) {
-			if (s[j - 1] == s[i - 1])
-		}
-	}
-}
-
-void solve(){
-    String sText; cin >> sText;
-    String sPattern; cin >> sPattern;
+    string sText; cin >> sText;
+    string sPattern; cin >> sPattern;
     int sizeText = sText.size();
     int sizePattern = sPattern.size();
+    if (sizeText < sizePattern) {
+        cout << "Not Possible: Pattern larger than text";
+    }
+
     sPattern = " " + sPattern;
-    cout << sPattern;
-    return;
+
     vector<int> v1(sizePattern + 1);
+
     createPrefixTable(sPattern, v1);
+    // fa (i, 1, sizePattern) {
 
-
-
-
-
-
-    
+    //     d1(v1[i]);
+    // }
 }
