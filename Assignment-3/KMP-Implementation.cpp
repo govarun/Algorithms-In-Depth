ac686c7404a9ad73ed75844368305bc451b26065
Varun Goyal <varungoyal23@gmail.com>
2019-06-17 16:26:12 +0530

    Code Updates: Implementation Completed

--- Assignment-3/KMP-Implementation.cpp
+++ Assignment-3/KMP-Implementation.cpp
@@ -30,8 +30,41 @@ void createPrefixTable(string s, vector<int> &v1) {
     }
 }
 
+bool checkPatternPresence (string sText, string sPattern, vector<int> v1) {
+    int sizeText = sText.size();
+    int sizePattern = sPattern.size();
+    for (int a = 0; a < sizeText; ++a) {
+        int i = 0;
+        while ((i + 1) < sizePattern) {
+            while (sPattern[i + 1] == sText[a] && (i + 1) < sizePattern && a < sizeText) {
+                if ((i + 1) == sizePattern - 1) {
+                  return true;  
+                }
+                ++i;
+                ++a;
+            }
+            if (a >= sizeText || i > sizePattern) {
+                    break;
+            }
+            if (sPattern[i + 1] != sText[a] && i == 0) {
+                break;
+            }
+            if (sPattern[i + 1] != sText[a]) {
+                i = v1[i];
+            }  
+        }    
+    }
+    return false;
+
+
+}
 
 int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
     string sText; cin >> sText;
     string sPattern; cin >> sPattern;
     int sizeText = sText.size();
@@ -40,13 +73,20 @@ int main(){
         cout << "Not Possible: Pattern larger than text";
     }
 
+    // sText = " " + sText;
     sPattern = " " + sPattern;
 
     vector<int> v1(sizePattern + 1);
-
+    for (int i = 0; i <= sizePattern; ++i) {
+        v1[i] = 0;
+    }
+    // cout << "Hello";
     createPrefixTable(sPattern, v1);
-    // fa (i, 1, sizePattern) {
-
-    //     d1(v1[i]);
+    // cout << "Hello";
+    // for (int i = 1; i <= sizePattern; ++i) {
+    //     cerr << (v1[i]) << endl;
     // }
+    // cout << checkPatternPresence(sText, sPattern, v1);
+    cout << checkPatternPresence(sText, sPattern, v1);
+
 }
