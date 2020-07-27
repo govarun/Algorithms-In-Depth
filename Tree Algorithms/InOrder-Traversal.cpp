07c846dd94b3ef9a44e01a285c1d14b799122583
Varun Goyal <varungoyal23@gmail.com>
2020-07-27 20:22:39 +0530

    Offline files pushed

---
+++ Tree Algorithms/InOrder-Traversal.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Author: Varun Goyal (@govarun)
+
+void func(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);    
+    cin.tie(0); cout.tie(0);
+    #ifdef GOVARUN
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #else 
+    #define GOVARUN 0
+    #endif
+    
+    func();
+}
+    
